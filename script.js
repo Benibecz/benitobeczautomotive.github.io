0dab1832ebb53903c8a98bfc90c392feed14796f
Benibecz <99119375+Benibecz@users.noreply.github.com>
2022-04-29 23:27:42 +0100

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+// const dropdown = document.querySelector(".dropdown");
+// const body = document.querySelector("body");
+// let width = screen.availWidth;
+
+// // if ((body.style.width = "540px")) {
+// //   dropdown.classList.remove("hidden");
+// // }
+// console.log(width);
